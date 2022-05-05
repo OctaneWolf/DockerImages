3f529e19cce5c69c3ad4ca175937b8e78e8053d4
Eric Pereyra <isopropilick@icloud.com>
2022-05-05 08:45:36 -0500

    Added Node-minimal

---
+++ Node-minimal/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = new express();
+let ip = "0.0.0.0";
+
+app.post('/publicip', function(req, res, ){
+   ip=req.originalUrl.slice(10)
+    res.json(ip)
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.send({ error: "Sorry, can't find that" })
+});
+app.listen(3000)
